1a5c0730a20619d70c074b16c60e9f988ee93df3
mir-asif01 <asifbsb2002@gmail.com>
2024-11-21 14:32:49 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { ref } from "vue"
+
+//compositions api
+const name = ref("mir kamrul ahsan asif")
+const status = ref("active")
+const tasks = ref(["task-1 composition api", "task-2", "task-3"])
+
+const changeStatusToActive = () => {
+  status.value = "active"
+}
+const changeStatusToInactive = () => {
+  status.value = "inactive"
+}
+const changeStatusToPending = () => {
+  status.value = "pending"
+}
+
+// return {
+//   name,
+//   status,
+//   tasks,
+//   changeStatusToActive,
+//   changeStatusToInactive,
+//   changeStatusToPending,
+// }
+
+//options api
+/* export default {
+  data() {
+    return {
+      message: "hello vue",
+      name: "Jon Jones",
+      status: "",
+      tasks: ["task-1", "task-2", "task-3"],
+      link: "https://google.com/",
+    }
+  },
+  methods: {
+    changeStatusToActive() {
+      this.status = "active"
+    },
+    changeStatusToInactive() {
+      this.status = "inactive"
+    },
+    changeStatusToPending() {
+      this.status = "pending"
+    },
+  },
+} */
+</script>
+
+<template>
+  <h2>to do app</h2>
+  <h1>{{ message }}</h1>
+  <h1>
+    {{ name }}
+    <span v-if="status === 'active'">is active</span>
+    <span v-else-if="status === 'inactive'">is inactive</span>
+    <span v-else-if="status === 'pending'">pending...</span>
+    <span v-else>status not found</span>
+  </h1>
+  <h2>Tasks</h2>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a :href="link">visit google</a>
+  <br />
+  <!-- <button v-on:click="changeStatusToActive">active</button> -->
+  <button @click="changeStatusToActive">active</button>
+  <button @click="changeStatusToInactive">inactive</button>
+  <button @click="changeStatusToPending">pending</button>
+</template>
+
+<style scoped>
+span {
+  color: blueviolet;
+}
+</style>
