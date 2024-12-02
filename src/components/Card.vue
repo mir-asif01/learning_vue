4bb8aa27cf13dcd67b437da5b5f07e5b6aafdb5d
mir-asif01 <asifbsb2002@gmail.com>
2024-12-02 09:29:22 +0600

    homecards added

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  divClasses: {
+    type: String,
+  },
+  title: {
+    type: String,
+    default: "card title",
+  },
+  subTitle: {
+    type: String,
+    default: "card subtitle",
+  },
+  btnText: {
+    type: String,
+    default: "Button",
+  },
+  titleClasses: {
+    type: String,
+    default: "",
+  },
+  subTitleClasses: {
+    type: String,
+    default: "",
+  },
+  btnClasses: {
+    type: String,
+    default: "",
+  },
+})
+</script>
+
+<template>
+  <div :class="divClasses">
+    <!-- <h2 class="text-2xl font-bold">{{ text }}</h2> -->
+    <h2 :class="titleClasses">{{ title }}</h2>
+    <p :class="subTitleClasses">
+      Browse our Vue jobs and start your career today
+    </p>
+    <a :class="btnClasses"> Browse Jobs </a>
+  </div>
+</template>
---
+++ src/components/HomeCards.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Card from "./Card.vue"
+</script>
+
+<template>
+  <section class="py-4">
+    <div class="container-xl lg:container m-auto">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+        <Card
+          div-classes="bg-gray-100 p-6 rounded-lg shadow-md"
+          title="For Developers"
+          sub-title="Browse our Vue jobs and start your career today"
+          btn-text="Browse Jobs"
+          title-classes="text-2xl font-bold"
+          sub-title-classes="mt-2 mb-4"
+          btn-classes="inline-block bg-green-500 text-white rounded-lg px-4 py-2 hover:bg-green-600"
+        />
+        <Card
+          div-classes="bg-green-100 p-6 rounded-lg shadow-md"
+          title="For Employees"
+          sub-title="List your jobs to find the perfect developer for the role"
+          btn-text="Add Job"
+          title-classes="text-2xl font-bold"
+          sub-title-classes="mt-2 mb-4"
+          btn-classes="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+        />
+      </div>
+    </div>
+  </section>
+</template>
